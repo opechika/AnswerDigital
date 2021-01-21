2dbae4adc0b26524c2a6a96df4ea7aa0333911a8
Chika Afolabi <opechika@yahoo.com>
2021-01-21 22:47:19 +0000

    first commit

---
+++ src/test/java/com/demoqa/feature/StudentCanFillRegistrationForm.feature
@@ -0,0 +1,28 @@
+Feature:Student Registration Form
+  As a Student
+  I want the ability to enter my details into the registration form
+  So that I can submit it
+
+  Scenario Outline: Student can successfully submit Registration Form
+    Given I am on Practice Form page
+    When I enter my "<FirstName>" into  first name field
+    And I enter my "<LastName>" into  last name field
+    And I enter my "<Email>" into email field
+    And I click on "<Gender>" as my gender
+    And I enter my "<MobileNumber>" into Mobile Number field
+    And I enter my "<DateOfBirth>" into Date of Birth field
+    And I enter "<Subject>" into subject field
+    And I click on "<Hobbies>" as my hobbies
+    And I "<Optional>" upload picture
+    And I enter my "<Address>" into current address field
+    And I select "<State>" from select state dropdown
+    And I select "<City>" from select city dropdown
+    And I submit the form with the details above
+    Then the details entered above are confirmed in a popup
+
+    Examples: Form Data
+      | FirstName     | LastName     | Email     | Gender | MobileNumber     | DateOfBirth | Subject     | Hobbies | Optional | Address     | State         | City |
+      | fakeFirstName | fakeLastName | fakeEmail | Male   | fakeMobileNumber | 24 May 1979 | fakeSubject | Sports  | False    | fakeAddress | Uttar Pradesh | Agra |
+
+
+
---
+++ src/test/java/com/demoqa/stepDefinitions/RegistrationFormSteps.java
@@ -0,0 +1,83 @@
+package com.demoqa.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class RegistrationFormSteps {
+    @Given("I am on Practice Form page")
+    public void i_am_on_practice_form_page() {
+
+    }
+
+    @When("I enter my {string} into  first name field")
+    public void i_enter_my_into_first_name_field(String string) {
+
+    }
+
+    @When("I enter my {string} into  last name field")
+    public void i_enter_my_into_last_name_field(String string) {
+
+    }
+
+    @When("I enter my {string} into email field")
+    public void i_enter_my_into_email_field(String string) {
+
+    }
+
+    @When("I click on {string} as my gender")
+    public void i_click_on_as_my_gender(String string) {
+
+    }
+
+    @When("I enter my {string} into Mobile Number field")
+    public void i_enter_my_into_mobile_number_field(String string) {
+
+    }
+
+    @When("I enter my {string} into Date of Birth field")
+    public void i_enter_my_into_date_of_birth_field(String string) {
+
+    }
+
+    @When("I enter {string} into subject field")
+    public void i_enter_into_subject_field(String string) {
+
+    }
+
+    @When("I click on {string} as my hobbies")
+    public void i_click_on_as_my_hobbies(String string) {
+
+    }
+
+    @When("I {string} upload picture")
+    public void i_upload_picture(String string) {
+
+    }
+
+    @When("I enter my {string} into current address field")
+    public void i_enter_my_into_current_address_field(String string) {
+
+    }
+
+    @When("I select {string} from select state dropdown")
+    public void i_select_from_select_state_dropdown(String string) {
+
+    }
+
+    @When("I select {string} from select city dropdown")
+    public void i_select_from_select_city_dropdown(String string) {
+
+    }
+
+    @When("I submit the form with the details above")
+    public void i_submit_the_form_with_the_details_above() {
+
+    }
+
+    @Then("the details entered above are confirmed in a popup")
+    public void the_details_entered_above_are_confirmed_in_a_popup() {
+
+    }
+
+}
